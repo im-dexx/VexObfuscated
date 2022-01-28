050db2edb23caa95cd804f1ee00e90df33f7054e
dex <97322316+im-dexx@users.noreply.github.com>
2022-01-27 21:37:09 -0500

    Add files via upload

--- pass_system.py
+++ pass_system.py
@@ -1,24 +1,14 @@
-import os
-import requests
-import time
+'''
+vex's useless obfuscator
+dex#1337 - <@623612991568478239>
+'''
 
-os.system("title very lame password system")
-I = "https"
-II = "://"
-III = "pastebin.com"
-IV = "/raw/"
-V = "4JanPfiQ"
-VI=I+II+III+IV+V
-VII=requests.get(VI).json()
-jj=VII["I"]+VII["II"]+VII["III"]
 
-def trycrack():
-    inputpass = input("Password: ")
-    if inputpass != jj:
-        print("incorrect password, retry\n")
-        trycrack()
-    elif inputpass == jj:
-        print("good job you got it correct.\nclosing.")
-        time.sleep(2)
-
-trycrack()
+'''hello there''';import json;import os;'''pfft loser''';import requests;import time;'''did you know vex>you''';os.system("title very lame password system");'''stop looking at my passwords''';IlIlIlIlIlIlIlIlIlIlllIl=requests.get;'''stop it...''';IlIlllIl="password:";'''isnt it a great password input?''';'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IlIlllIlIl="incorrect password, retry\n";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';'''kinda bored.''';IlIIllIlIlI="good job you got it correct.\nclosing.";'''yoo.. you did it right?''';'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IlIlIlIlIllllIIl="://paste";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IlIlIlIIIIlllIIl="bin.com";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IlIIIlIlIIlllIIl="https";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IIIIIlIlIllllIIl="4JanPfiQ";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IIIlIlIlIIlllIIl="/raw/";'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';IlIlIlIlIlIlIlllI=IlIlIlIlIlIlIlIlIlIlllIl(IlIIIlIlIIlllIIl+IlIlIlIlIllllIIl+IlIlIlIIIIlllIIl+IIIlIlIlIIlllIIl+IIIIIlIlIllllIIl).json();IlIlIl=IlIlIlIlIlIlIlllI;IlIlIlIlIlIlIllll=IlIlIlIlIlIlIlllI["IlIlIlIlIlllIlllI"]+IlIlIlIlIlIlIlllI["IlIIIlIlIlIlIlllI"];'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl'''
+def IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl():
+    IlIlIlIlI=input(f"{IlIlllIl} ")
+    if IlIlIlIlI!=IlIlIlIlIlIlIllll:
+        '''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';print(f"{IlIlllIlIl}");IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl();'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl'''
+    elif IlIlIlIlI==IlIlIlIlIlIlIllll:
+        '''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl''';print(f"{IlIIllIlIlI}");time.sleep(2);'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl'''
+IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl();'''IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl'''
